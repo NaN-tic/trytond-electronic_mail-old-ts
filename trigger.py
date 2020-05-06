d7310990a663253e383802a4f81616c64df1fefb
Raimon Esteve <raimonesteve@gmail.com>
2020-05-06 16:29:00 +0200

    Simplify trigger action issue9096

--- trigger.py
+++ trigger.py
@@ -12,10 +12,15 @@ class Trigger(metaclass=PoolMeta):
     __name__ = 'ir.trigger'
     email_template = fields.Many2One('electronic.mail.template', 'Template')
 
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls.action.selection.append(
+            ('electronic.mail.template|mail_from_trigger', "Email Template"),
+            )
+
     @staticmethod
     def default_model():
-        Model = Pool().get('ir.model')
-
         model = Transaction().context.get('model', None)
         if model:
             return model
